43da73cc1ec7f0eeb00865081cd850bc1b16c213
sardarSanyukta <147426821+sardarSanyukta@users.noreply.github.com>
2023-11-20 09:28:30 +0530

    Delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-#This code prints the number of characters in a user's name.
-print( len( input("What is your name? ") ) )
-
-
-
-#Notes
-#If input was "Jack"
-#1st: print(len("Jack"))
-#2nd: print(4)
